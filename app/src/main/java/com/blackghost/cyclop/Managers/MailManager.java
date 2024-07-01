d8956c6be4447ccfc8568b3979f5a41bf48950e4
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-07-01 11:15:58 +0200

    Mod MailManager

--- app/src/main/java/com/blackghost/cyclop/Managers/MailManager.java
+++ app/src/main/java/com/blackghost/cyclop/Managers/MailManager.java
@@ -1,4 +1,17 @@
 package com.blackghost.cyclop.Managers;
 
 public class MailManager {
+    private String username;
+    private String password;
+
+    public MailManager(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean sendEmail(){
+        return false;
+    }
+    public void fetchEmails(){
+    }
 }
