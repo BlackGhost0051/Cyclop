3d208d0abdef5370afab836abb000178a9d680f7
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2024-07-02 19:55:39 +0200

    Add structure

--- app/src/main/java/com/blackghost/cyclop/MainActivity.java
+++ app/src/main/java/com/blackghost/cyclop/MainActivity.java
@@ -2,6 +2,9 @@ package com.blackghost.cyclop;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,14 +16,33 @@ import com.blackghost.cyclop.Class.Info;
 import com.blackghost.cyclop.Managers.MailManager;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText inputMail;
+    private EditText inputPass;
+    private Button saveBtn;
     private MailManager mailManager;
     private Info info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         info = new Info();
+
+        inputMail = findViewById(R.id.inputMail);
+        inputPass = findViewById(R.id.inputPass);
+        saveBtn = findViewById(R.id.saveBtn);
+
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                info.setEmail(String.valueOf(inputMail.getText()));
+                info.setPassword(String.valueOf(inputPass.getText()));
+            }
+        });
+
+
+
         mailManager = new MailManager(info.getEmail(), info.getPassword());
         mailManager.fetchEmails(5);
     }
